8204873617ee5ef8c84bdacbc313221bf805ccde
Rikesh Dhokia <rikeshdhokia@gmail.com>
2016-06-27 23:12:50 +1000

    Write some rows to messages table

--- src/go.rb
+++ src/go.rb
@@ -1,10 +1,21 @@
 require_relative 'rds_connection'
+require 'securerandom'
+require 'forgery'
 
-DB = RdsConnection.new
+DB = RdsConnection.new.connect()
 
-p DB.inspect
+# DB.create_table :messages do
+#   primary_key :id, :uuid
+#   column :title, :text
+#   String :content
+#   index :title
+# end
 
-# messages = DB[:messages]
+messages = DB[:messages]
 
-# p messages
+
+(1..1000).each {
+  out = messages.insert({ id: SecureRandom.uuid, title: Forgery::Name.full_name, content: Forgery('lorem_ipsum').text})
+  puts out
+}
 
--- src/rds_connection.rb
+++ src/rds_connection.rb
@@ -1,10 +1,15 @@
+require 'dotenv'
+require 'sequel'
+
 class RdsConnection
 
-  def initialise()
+  def initialize()
       Dotenv.load
+  end
 
-      Sequel.postgres(ENV['DB_NAME'], user: ENV['DB_USER'],
-        password: ENV['DB_PASSWORD'], host: ENV['DB_HOST'], port: ENV['DB_PORT'])
-    end
+  def connect
+    Sequel.connect(:adapter=>'postgres', :database => ENV['DB_NAME'], :user => ENV['DB_USER'],
+        :password => ENV['DB_PASSWORD'], :host =>ENV['DB_HOST'], :port=>ENV['DB_PORT'])
+  end
 
 end
